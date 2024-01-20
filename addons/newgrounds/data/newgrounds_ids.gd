da7f068699977095b705ce0859e07b2d604a5b27
jefvel <akselkornesjo@gmail.com>
2024-01-20 21:25:54 +0100

    cleaned up

--- addons/newgrounds/data/newgrounds_ids.gd
+++ addons/newgrounds/data/newgrounds_ids.gd
@@ -6,32 +6,15 @@ class_name NewgroundsIds
 
 enum ScoreboardId {
 	None = 0,
-	TimeToWin = 13398,
 
 }
 
 enum MedalId {
 	None = 0,
-	Lost = 76570,
-	_5Up = 76573,
-	BeatGame = 76569,
-	Twenty = 76574,
-	Fifty = 76575,
-	LaserHit = 76617,
-	Heartful = 76572,
-	MegaBeat = 76571,
 
 }
 
 class MedalIdsToResource:
 	const medals = {
-		76570: "res://addons/newgrounds/imported_medals/76570.tres",
-		76573: "res://addons/newgrounds/imported_medals/76573.tres",
-		76569: "res://addons/newgrounds/imported_medals/76569.tres",
-		76574: "res://addons/newgrounds/imported_medals/76574.tres",
-		76575: "res://addons/newgrounds/imported_medals/76575.tres",
-		76617: "res://addons/newgrounds/imported_medals/76617.tres",
-		76572: "res://addons/newgrounds/imported_medals/76572.tres",
-		76571: "res://addons/newgrounds/imported_medals/76571.tres",
 
 	}
